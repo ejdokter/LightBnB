2105667d11778994b8db39dd0d776bdac21baa72
Eric Dokter <edokter11@gmail.com>
2022-04-20 11:18:10 -0600

    refactor getAllProperties function to accept search options from database

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -94,22 +94,60 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  return pool.query(`
-    SELECT * 
-    FROM properties
-    LIMIT $1
-  `, [limit])
-  .then((res) => {
+
+  const queryParams = [];
+
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  `;
+
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city LIKE $${queryParams.length}`;
+  }
+
+  if (options.minimum_price_per_night) {
+    queryParams.push(options.minimum_price_per_night * 100)
+    if (queryParams.length > 1) {
+      queryString += `AND cost_per_night >= $${queryParams.length}`
+    } else {
+      queryString += `WHERE cost_per_night >= $${queryParams.length}`
+    }
+  }
+
+  if (options.maximum_price_per_night) {
+    queryParams.push(options.maximum_price_per_night * 100)
+    if (queryParams.length > 1) {
+      queryString += `AND cost_per_night <= $${queryParams.length}`
+    } else {
+      queryString += `WHERE cost_per_night <= $${queryParams.length}`
+    }
+  }
+
+  if (options.minimum_rating) {
+    queryParams.push(options.minimum_rating)
+    if (queryParams.length > 1) {
+      queryString += `AND property_reviews.rating >= $${queryParams.length}`
+    } else {
+      queryString += `WHERE property_reviews.rating >= $${queryParams.length}`
+    }
+  }
+
+  queryParams.push(limit)
+  queryString += `
+  GROUP BY properties.id
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `
+
+  console.log(queryString, queryParams, options)
+
+  return pool.query(queryString, queryParams).then((res) => {
     return res.rows
   })
-  .catch((err) => {
-    console.log(err.message)
-  })
-  // const limitedProperties = {};
-  // for (let i = 1; i <= limit; i++) {
-  //   limitedProperties[i] = properties[i];
-  // }
-  // return Promise.resolve(limitedProperties);
+
 }
 exports.getAllProperties = getAllProperties;
 
